9c9612c5f16c95998f7ad8df0af114185cfed04c
Ammar Miyaji <ammarmiyaji786@gmail.com>
2018-09-30 17:33:20 +0530

    sync it now bro

--- app/src/main/java/com/ammar/saifiyahschool/Results.java
+++ app/src/main/java/com/ammar/saifiyahschool/Results.java
@@ -1,14 +1,51 @@
 package com.ammar.saifiyahschool;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-import android.os.Bundle;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
 import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.TextView;
 
-public class Results extends Activity {
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
+public class Results extends AppCompatActivity  {
+
+    private WebView webView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(new TableMainLayout(this));
+        setContentView(R.layout.activity_results);
+
+        webView = (WebView)findViewById(R.id.myWebView);
+        webView.setWebViewClient(new WebViewClient());
+
+        //Put your URL our here:
+        webView.loadUrl("http://www.google.com");
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
     }
 }
